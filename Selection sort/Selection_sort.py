eda414170de7d994048786cf71e898303e6eaa3c
amit <amitsarker95@gmail.com>
2024-08-22 16:13:31 +0600

    complete

--- Selection sort/Selection_sort.py
+++ Selection sort/Selection_sort.py
@@ -8,7 +8,7 @@ arr = [7,3,5,2,1,6]
 def selection_sort(arr):
     for i in range(len(arr)):
         min_value = min(arr[i:])
-        min_index = arr.index(min_value)
+        min_index = arr.index(min_value, i)
         tmp = arr[i]
         arr[i] = arr[min_index]
         arr[min_index] = tmp
