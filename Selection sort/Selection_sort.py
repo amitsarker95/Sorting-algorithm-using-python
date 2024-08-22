75792d37f2a804879298961bd39fb40eebbb297d
amit <amitsarker95@gmail.com>
2024-08-22 16:20:53 +0600

    more optimized

--- Selection sort/Selection_sort.py
+++ Selection sort/Selection_sort.py
@@ -9,9 +9,10 @@ def selection_sort(arr):
     for i in range(len(arr)-1):
         min_value = min(arr[i:])
         min_index = arr.index(min_value, i)
-        tmp = arr[i]
-        arr[i] = arr[min_index]
-        arr[min_index] = tmp
+        if arr[i] != arr[min_index]:
+            tmp = arr[i]
+            arr[i] = arr[min_index]
+            arr[min_index] = tmp
     return arr
 
 print(selection_sort(arr))
