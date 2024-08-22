615268651631ec48c65f2f09822a1f0a5d667038
amit <amitsarker95@gmail.com>
2024-08-22 16:07:44 +0600

    Selection sort added

---
+++ Selection sort/Selection_sort.py
@@ -0,0 +1,19 @@
+'''
+Selection Sort Algorithm using min()
+
+'''
+
+arr = [7,3,5,2,1,6]
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_value = min(arr[i:])
+        min_index = arr.index(min_value)
+        tmp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index] = tmp
+    return arr
+
+print(selection_sort(arr))
+
+
